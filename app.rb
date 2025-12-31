019c7ca1e2b3eebb8280587038cc2d8e66520b60
NipulK <168514285+NipulK@users.noreply.github.com>
2025-12-31 23:48:06 +0530

    update app.rb to connect DB

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
+require './models/student'
 
 set :bind, '0.0.0.0'
 
-students = []
-
 get '/' do
-  @students = students
+  @students = Student.all
   erb :index
 end
 
@@ -14,23 +13,21 @@ get '/new' do
 end
 
 post '/create' do
-  students << { id: Time.now.to_i, name: params[:name], age: params[:age] }
+  Student.create(params[:name], params[:age])
   redirect '/'
 end
 
 get '/edit/:id' do
-  @student = students.find { |s| s[:id].to_s == params[:id] }
+  @student = Student.find(params[:id])
   erb :edit
 end
 
 post '/update/:id' do
-  s = students.find { |x| x[:id].to_s == params[:id] }
-  s[:name] = params[:name]
-  s[:age] = params[:age]
+  Student.update(params[:id], params[:name], params[:age])
   redirect '/'
 end
 
 get '/delete/:id' do
-  students.reject! { |s| s[:id].to_s == params[:id] }
+  Student.delete(params[:id])
   redirect '/'
 end
