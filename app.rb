ec4e908cbcba3e55202325302d35707ad3c1a79d
NipulK <168514285+NipulK@users.noreply.github.com>
2026-01-01 16:51:09 +0530

    feat: implement user authentication with login and logout functionality

--- app.rb
+++ app.rb
@@ -1,11 +1,39 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require './models/student'
+require './models/user'
 
 class App < Sinatra::Base
+  enable :sessions
+  set :session_secret, "student_secret_key"
   set :bind, '0.0.0.0'
   set :port, 4567
 
+  before do
+    redirect '/login' unless session[:user] || request.path_info == '/login'
+  end
+
+  # ---------- AUTH ----------
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.authenticate(params[:username], params[:password])
+    if user
+      session[:user] = user[:username]
+      redirect '/'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  # ---------- STUDENT CRUD ----------
   get '/' do
     @students = Student.all
     erb :index
