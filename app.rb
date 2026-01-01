75ffa92b80d366b858a82b864d34a9c635cd24b6
NipulK <168514285+NipulK@users.noreply.github.com>
2026-01-01 11:20:13 +0530

    refactor: update Sinatra require and restructure App class

--- app.rb
+++ app.rb
@@ -1,37 +1,39 @@
 require 'bundler/setup'
-require 'sinatra'
+require 'sinatra/base'
 require './models/student'
 
+class App < Sinatra::Base
+  set :bind, '0.0.0.0'
+  set :port, 4567
 
-set :bind, '0.0.0.0'
+  get '/' do
+    @students = Student.all
+    erb :index
+  end
 
-get '/' do
-  @students = Student.all
-  erb :index
+  get '/new' do
+    erb :new
+  end
+
+  post '/create' do
+    Student.create(params[:name], params[:age])
+    redirect '/'
+  end
+
+  get '/edit/:id' do
+    @student = Student.find(params[:id])
+    erb :edit
+  end
+
+  post '/update/:id' do
+    Student.update(params[:id], params[:name], params[:age])
+    redirect '/'
+  end
+
+  get '/delete/:id' do
+    Student.delete(params[:id])
+    redirect '/'
+  end
+
+  run! if app_file == $0
 end
-
-get '/new' do
-  erb :new
-end
-
-post '/create' do
-  Student.create(params[:name], params[:age])
-  redirect '/'
-end
-
-get '/edit/:id' do
-  @student = Student.find(params[:id])
-  erb :edit
-end
-
-post '/update/:id' do
-  Student.update(params[:id], params[:name], params[:age])
-  redirect '/'
-end
-
-get '/delete/:id' do
-  Student.delete(params[:id])
-  redirect '/'
-end
-
-
