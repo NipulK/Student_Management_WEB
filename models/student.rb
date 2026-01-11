18134dff498e7c94c1b3a911a1dab923a1c8f55b
NipulK <168514285+NipulK@users.noreply.github.com>
2026-01-11 14:27:34 +0530

    feat: refactor Student model to improve data handling and structure

--- models/student.rb
+++ models/student.rb
@@ -16,6 +16,60 @@ class Student
   def self.find(id)
     COL.find(_id: BSON::ObjectId(id)).first
   end
+require 'mongo'
+Mongo::Logger.logger.level = ::Logger::FATAL
+
+client = Mongo::Client.new(['127.0.0.1:27017'], database: 'student_web')
+COL = client[:students]
+
+class Student
+
+  def self.all
+    COL.find.to_a
+  end
+
+  def self.create(data)
+    COL.insert_one({
+      name: data[:name],
+      age: data[:age],
+      address: data[:address],
+      mobile: data[:mobile],
+      nic: data[:nic],
+      school: data[:school]
+    })
+  end
+
+  def self.find(id)
+    COL.find(_id: BSON::ObjectId(id)).first
+  end
+
+  def self.update(id, data)
+    COL.find(_id: BSON::ObjectId(id)).update_one(
+      '$set' => {
+        name: data[:name],
+        age: data[:age],
+        address: data[:address],
+        mobile: data[:mobile],
+        nic: data[:nic],
+        school: data[:school]
+      }
+    )
+  end
+
+  def self.delete(id)
+    COL.find(_id: BSON::ObjectId(id)).delete_one
+  end
+
+  def self.search(keyword)
+    COL.find({
+      '$or' => [
+        { name: /#{keyword}/i },
+        { mobile: /#{keyword}/i },
+        { school: /#{keyword}/i }
+      ]
+    }).to_a
+  end
+end
 
   def self.update(id, name, age)
     COL.find(_id: BSON::ObjectId(id))
